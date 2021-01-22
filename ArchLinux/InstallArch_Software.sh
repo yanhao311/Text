b0b77666f5d7918060ab6404183cd09f9e09f071
yanhao311 <yanhao311@126.com>
2021-01-22 16:27:58 +0800

    arch操作系统安装脚本更新

--- ArchLinux/InstallArch_Software.sh
+++ ArchLinux/InstallArch_Software.sh
@@ -15,13 +15,16 @@ pacman -S --noconfirm alsa-utils
 # 安装并配置显卡驱动
 pacman -S --noconfirm xf86-video-vmware
 
+# 中文字体
+pacman -S --noconfirm wqy-zenhei
+
 # 桌面环境
 pacman -S --noconfirm xorg xorg-xrandr deepin
 sed 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-deepin-greeter/g' /etc/lightdm/lightdm.conf
 systemctl enable lightdm
 # 配置分辨率
 echo "xrandr --newmode \"1920x1080_60.00\" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync" >> /etc/profile
-echo "xrandr --addmode Virtual-1 \"1920x1080_60.00\"" >> /etc/profile
+echo "xrandr --addmode Virtual1 \"1920x1080_60.00\"" >> /etc/profile
 
 # 命令行工具
 pacman -S --noconfirm deepin-terminal
@@ -31,3 +34,23 @@ pacman -S --noconfirm git
 git config --global user.name "yanhao311"
 git config --global user.email "yanhao311@126.com"
 git config --global core.quotepath false
+mkdir /home/yanhao/.ssh
+cp ./id_rsa /home/yanhao/.ssh/id_rsa
+
+# 添加archlinuxcn源
+echo "[archlinuxcn]" >> /etc/pacman.conf
+echo "SigLevel = Optional TrustAll" >> /etc/pacman.conf
+echo "Server = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
+pacman -Sy
+pacman -S --noconfirm archlinuxcn-keyring
+
+# 安装google浏览器
+pacman -S --noconfirm google-chrome
+
+# 安装中文输入法
+pacman -S --noconfirm fcitx5-im fcitx5-chinese-addons fcitx5-pinyin-zhwiki
+echo "INPUT_METHOD  DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
+echo "GTK_IM_MODULE DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
+echo "QT_IM_MODULE  DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
+echo "SDL_IM_MODULE DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
+echo "XMODIFIERS    DEFAULT=@im=fcitx5" >> /home/yanhao/.pam_environment
