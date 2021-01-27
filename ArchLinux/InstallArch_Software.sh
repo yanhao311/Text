1845fe3e8c8b2bd8a7fd5fcf95d6df76d9904dc5
yanhao311 <yanhao311@126.com>
2021-01-27 19:24:19 +0800

    aa

--- ArchLinux/InstallArch_Software.sh
+++ ArchLinux/InstallArch_Software.sh
@@ -18,16 +18,19 @@ pacman -S --noconfirm xf86-video-vmware
 # 中文字体
 pacman -S --noconfirm wqy-zenhei
 
-# 桌面环境
-pacman -S --noconfirm xorg xorg-xrandr deepin
-sed 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-deepin-greeter/g' /etc/lightdm/lightdm.conf
-systemctl enable lightdm
+# deepin桌面环境
+# pacman -S --noconfirm xorg xorg-xrandr deepin
+# sed 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-deepin-greeter/g' /etc/lightdm/lightdm.conf
+# systemctl enable lightdm
+# KDE桌面环境
+pacman -S xorg plasma plasma-wayland-session
+systemctl enable sddm.service
 # 配置分辨率
 echo "xrandr --newmode \"1920x1080_60.00\" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync" >> /etc/profile
 echo "xrandr --addmode Virtual1 \"1920x1080_60.00\"" >> /etc/profile
 
 # 命令行工具
-pacman -S --noconfirm deepin-terminal
+pacman -S --noconfirm yakuake
 
 # 安装配置git
 pacman -S --noconfirm git
@@ -48,9 +51,9 @@ pacman -S --noconfirm archlinuxcn-keyring
 pacman -S --noconfirm google-chrome
 
 # 安装中文输入法
-pacman -S --noconfirm fcitx5-im fcitx5-chinese-addons fcitx5-pinyin-zhwiki
-echo "INPUT_METHOD  DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
-echo "GTK_IM_MODULE DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
-echo "QT_IM_MODULE  DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
-echo "SDL_IM_MODULE DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
-echo "XMODIFIERS    DEFAULT=@im=fcitx5" >> /home/yanhao/.pam_environment
+# pacman -S --noconfirm fcitx5-im fcitx5-chinese-addons fcitx5-pinyin-zhwiki
+# echo "INPUT_METHOD  DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
+# echo "GTK_IM_MODULE DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
+# echo "QT_IM_MODULE  DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
+# echo "SDL_IM_MODULE DEFAULT=fcitx5" >> /home/yanhao/.pam_environment
+# echo "XMODIFIERS    DEFAULT=@im=fcitx5" >> /home/yanhao/.pam_environment
